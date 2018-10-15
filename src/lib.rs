4280b6047c782778f20cac0f8047617929944a78
Nick Hazen <nicholas@sweetlabs.com>
2018-10-15 01:36:02 -0700

    Session cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -41,3 +41,35 @@ pub fn get_object_list(client: S3Client, bucket: &str) -> Result<Vec<String>, &'
 pub fn filter_object(name: &String, filter: &String) -> bool {
     name.contains(filter)
 }
+
+//
+//    let client = S3Client::new(Region::UsEast1);
+
+//    let mut list_request = ListObjectsRequest::default();
+//    list_request.bucket = String::from("pokki-analytics-raw");
+//
+//    let list_result = client.list_objects(list_request).sync().unwrap();
+//
+//    for object in list_result.contents.unwrap().iter() {
+//        println!("Object Key: {}", object.key.clone().unwrap());
+//        println!("Size: {}", object.size.clone().unwrap());
+//    }
+//
+//    // Get Object
+//    let mut object_request = GetObjectRequest::default();
+//    object_request.bucket = String::from("pokki-analytics-raw");
+//    object_request.key = String::from("nginx/analytics_capture-access_log.2018100516-1538755201.log-4pslTonUOb4G.gz");
+//
+//    let object_result = client.get_object(object_request).sync().unwrap();
+//
+//
+//    let stream = object_result.body.unwrap();
+//    let body = stream.concat2().wait().unwrap();
+//
+//    let b = body.as_slice();
+//
+//    let mut d = GzDecoder::new(b);
+//    let mut s = String::new();
+//    d.read_to_string(&mut s).unwrap();
+//
+//    println!("Body: {}", s);
--- src/main.rs
+++ src/main.rs
@@ -15,40 +15,18 @@ use futures::{Future, Stream};
 use flate2::read::GzDecoder;
 use std::str;
 
+//TODO MVP - Add documents
+//TODO MVP - Iterate through multiple object lists
+//TODO MVP - Create file streaming functions
+//TODO MVP - Create Search funtionality
+//TODO MVP - Create Display functionality
+//TODO MVP - Cleanup error handling
+//TODO MVP - First round of refactoing
+//TODO Experiment - Accecpt javascript for filter expression
+
 fn main() {
     let yaml = load_yaml!("../cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
     s3arch::run(matches);
-//
-//    let client = S3Client::new(Region::UsEast1);
-
-//    let mut list_request = ListObjectsRequest::default();
-//    list_request.bucket = String::from("pokki-analytics-raw");
-//
-//    let list_result = client.list_objects(list_request).sync().unwrap();
-//
-//    for object in list_result.contents.unwrap().iter() {
-//        println!("Object Key: {}", object.key.clone().unwrap());
-//        println!("Size: {}", object.size.clone().unwrap());
-//    }
-//
-//    // Get Object
-//    let mut object_request = GetObjectRequest::default();
-//    object_request.bucket = String::from("pokki-analytics-raw");
-//    object_request.key = String::from("nginx/analytics_capture-access_log.2018100516-1538755201.log-4pslTonUOb4G.gz");
-//
-//    let object_result = client.get_object(object_request).sync().unwrap();
-//
-//
-//    let stream = object_result.body.unwrap();
-//    let body = stream.concat2().wait().unwrap();
-//
-//    let b = body.as_slice();
-//
-//    let mut d = GzDecoder::new(b);
-//    let mut s = String::new();
-//    d.read_to_string(&mut s).unwrap();
-//
-//    println!("Body: {}", s);
 }
